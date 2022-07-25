07a20d8ce002fff4ced9f63fe240ba19c2696786
mshtelma <mshtelma>
2022-07-25 09:04:39 +0000

    initial commit

--- notebooks/databricks.py
+++ notebooks/databricks.py
@@ -1,17 +1,20 @@
 # Databricks notebook source
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+
+# COMMAND ----------
 
 # MAGIC %md
 # MAGIC # MLflow Regression Pipeline Databricks Notebook
 # MAGIC This notebook runs the MLflow Regression Pipeline on Databricks and inspects its results.
-# MAGIC
+# MAGIC 
 # MAGIC For more information about the MLflow Regression Pipeline, including usage examples,
 # MAGIC see the [Regression Pipeline overview documentation](https://mlflow.org/docs/latest/pipelines.html#regression-pipeline)
 # MAGIC and the [Regression Pipeline API documentation](https://mlflow.org/docs/latest/python_api/mlflow.pipelines.html#module-mlflow.pipelines.regression.v1.pipeline).
 
 # COMMAND ----------
 
-# MAGIC %pip install mlflow[pipelines]
-# MAGIC %pip install -r ../requirements.txt
+#%pip install -U ../mlflow-1.26.2.dev0-py3-none-any.whl[pipelines] pandas-profiling
 
 # COMMAND ----------
 
@@ -37,7 +40,52 @@ p.run("split")
 
 # COMMAND ----------
 
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, MinMaxScaler
+
+from mlflow.pipelines.decorators import transformer_method
+
+
+@transformer_method
+def transform_method():
+
+    return Pipeline(
+        steps=[
+            (
+                "encoder",
+                ColumnTransformer(
+                    transformers=[
+                        (
+                            "hour_encoder",
+                            OneHotEncoder(categories="auto", sparse=False),
+                            ["pickup_hour", "pickup_dow"],
+                        ),
+                        (
+                            "minmax_scaler",
+                            MinMaxScaler(),
+                            ["trip_distance", "trip_duration"],
+                        ),
+                    ]
+                ),
+            ),
+        ]
+    )
+
+# COMMAND ----------
+
 p.run("transform")
+
+# COMMAND ----------
+
+import sklearn.linear_model
+from mlflow.pipelines.decorators import estimator_method
+
+
+@estimator_method
+def custom_regressor():
+    #return sklearn.linear_model.LinearRegression()
+    return sklearn.linear_model.ElasticNet()
 
 # COMMAND ----------
 
@@ -64,3 +112,11 @@ test_data.describe()
 
 trained_model = p.get_artifact("model")
 print(trained_model)
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/databricks-datasets
+
+# COMMAND ----------
+
+
--- steps/train.py
+++ steps/train.py
@@ -1,17 +1,14 @@
-"""
-This module defines the following routines used by the 'train' step of the regression pipeline:
-
-- ``estimator_fn``: Defines the customizable estimator type and parameters that are used
-  during training to produce a model pipeline.
-"""
 
 
-def estimator_fn():
-    """
-    Returns an *unfitted* estimator that defines ``fit()`` and ``predict()`` methods.
-    The estimator's input and output signatures should be compatible with scikit-learn
-    estimators.
-    """
-    from sklearn.linear_model import SGDRegressor
+  
+def estimator_lr():
+  from sklearn.linear_model import LinearRegression
+  return LinearRegression()
 
-    return SGDRegressor(random_state=42)
+
+def estimator_sgd():
+  from sklearn.linear_model import SGDRegressor
+  return SGDRegressor(random_state=42)
+  
+  
+
